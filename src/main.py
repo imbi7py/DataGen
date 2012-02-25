05365f605d4bb849f2861faf77e32943e25f6037
Mahmoud Hashemi <makuro@gmail.com>
2012-02-25 05:05:13 -0800

    implemented streaming gzip using pipes.

--- src/main.py
+++ src/main.py
@@ -8,9 +8,13 @@ import Queue
 import random
 import sys
 
+import fcntl
+import errno
+import select
+
 from functools import wraps
 
-MP_ENABLED = True
+MP_ENABLED = False
 MAX_WORKERS = 10
 COMP_LEVEL = 9
 
@@ -27,35 +31,30 @@ try:
 except ImportError as ie:
   S3_ENABLED = False
 
-if S3_ENABLED:
-  # a decorator for saving to s3. could probably be enhanced
-  # TODO: untested
-  def save_to_s3(s3_prefix, s3_bucket):
-    def decorator(f):
-      @wraps(f)
-      def g(*args, **kwargs):
-        s3 = boto.connect_s3()
-        bucket = boto.s3.bucket.Bucket(s3, s3_bucket)
-        output_path = kwargs.get('outputFilePath') or args[1]
-        # TODO: better pattern for the above?
-      
-        md5_digest, _, _ = f(*args, **kwargs)
-        
-        key = boto.s3.key.Key(bucket)
-        key.key = s3_prefix + "/" + output_path
-        digest = key.get_md5_from_hexdigest(md5_digest)
-        key.set_contents_from_filename(output_path, md5=digest)
-        os.remove(output_path)
-        
-        return md5_digest
-      return g
-    return decorator
-else:
-  # dummy wrapper
-  def save_to_s3(*args, **kwargs):
-    def decorator(f):
+# a decorator for saving to s3. could probably be enhanced
+# TODO: untested
+def save_to_s3(s3_prefix, s3_bucket):
+  def decorator(f):
+    if not S3_ENABLED:
       return f
-    return decorator
+    @wraps(f)
+    def g(*args, **kwargs):
+      s3 = boto.connect_s3()
+      bucket = boto.s3.bucket.Bucket(s3, s3_bucket)
+      output_path = kwargs.get('outputFilePath') or args[1]
+      # TODO: better pattern for the above?
+      
+      md5_digest, _, _ = f(*args, **kwargs)
+      
+      key = boto.s3.key.Key(bucket)
+      key.key = s3_prefix + "/" + output_path
+      digest = key.get_md5_from_hexdigest(md5_digest)
+      key.set_contents_from_filename(output_path, md5=digest)
+      os.remove(output_path)
+        
+      return md5_digest
+    return g
+  return decorator
 
 #TODO: not needed
 def md5Digest(filePath):
@@ -68,14 +67,71 @@ def md5Digest(filePath):
   infile.close()
   return checksum.hexdigest()
 
-@save_to_s3(S3_PREFIX, S3_BUCKET)
-def makeRandomFile(numLines, outputFilePath, hashtype='md5', comp_level=9):
-  random.seed()
-  outfile = gzip.open(outputFilePath, 'wb', comp_level)
-  hashemi = hashlib.__dict__.get(hashtype)()
+class GzipStreamFile(object):
+  def __init__(self, filename, comp_level=9, out_hook=None):
+    if out_hook and not callable(out_hook):
+      raise TypeError("out_hook must be a callable.")
+    self.out_hook = out_hook
 
-  bytes_written      = 0
-  comp_bytes_written = 0
+    (fd_r, fd_w) = os.pipe() # Try a pipe. Could use cStringIO for the same.
+    fcntl.fcntl(fd_r, fcntl.F_SETFL, os.O_NONBLOCK)
+    self.pipe_r = os.fdopen(fd_r, 'rb')
+    self.pipe_w = os.fdopen(fd_w, 'wb')
+
+    self.gzip_pipe = gzip.GzipFile(fileobj=self.pipe_w, compresslevel=comp_level)
+    self.outfile = open(filename, 'wb')
+
+    self.bytes_written = 0
+    self.comp_bytes_written = 0
+
+  def write(self, data):
+    self.bytes_written += self.gzip_pipe.write(data)
+    try:
+      comp_data = self.pipe_r.read()
+      self.outfile.write(comp_data)
+
+      comp_bytes_written = len(comp_data)
+      self.comp_bytes_written += comp_bytes_written
+      return comp_bytes_written
+    except IOError as ioe:
+      if ioe.errno == errno.EAGAIN:
+        return 0
+      else:
+        raise
+
+  def close(self, abandon=False):
+    self.gzip_pipe.close()
+
+    # This is being extra cautious. The close() above should flush and this
+    # select should almost always return immediately and may not actually
+    # be neceessary.
+    if not abandon:
+      rd,_,_ = select.select([self.pipe_r],[],[self.pipe_r], 1)
+      if rd:
+        comp_data = self.pipe_r.read()
+        self.out_hook(comp_data)
+        self.outfile.write(comp_data)
+
+    self.pipe_w.close()
+    self.pipe_r.close()
+    self.outfile.close()
+
+  def __del__(self):
+    self.close(abandon=True)
+
+  def flush(self):
+    pass
+    # Gzipfile doesn't expose this, but it's totally possible
+    # to close the gzip_pipe to flush it and make a new one.
+
+@save_to_s3(S3_PREFIX, S3_BUCKET)
+def makeRandomFile(numLines, outputFilePath, hash_type='md5', comp_level=9):
+  random.seed()
+
+  hashemi = hashlib.__dict__.get(hash_type)()
+  gzip_file = GzipStreamFile(outputFilePath, 
+                             comp_level=comp_level, 
+                             out_hook=hashemi.update)
 
   for i in range(numLines):
     seed1 = random.randint(0,2**32-1)
@@ -101,46 +157,27 @@ def makeRandomFile(numLines, outputFilePath, hashtype='md5', comp_level=9):
              seed7 * seed1
              )
 
-    #outString = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}\t{11}\t{12}\t{13}\n".format(a,b,c,d,e,f,g,h,i,j,k,l,m,n)
-    # alt to above ^
-    # outString = "{a}\t{b}\t{c}\n".format(**locals())
     outline = "\t".join([str(v) for v in vals])+"\n"
-    hashemi.update(outline)
-    bytes_written += outfile.write(outline)
+    gzip_file.write(outline)
   
-  outfile.close()
-  comp_bytes_written = os.path.getsize(outputFilePath)
-  return hashemi.hexdigest(), bytes_written, comp_bytes_written
-
-def processWork(queue):
-  while True:
-    try:
-      (numLines, outputFilePath, bucket, prefix) = queue.get(True, 3)
-    except Queue.Empty:
-      continue
-    if numLines == 0:
-      queue.task_done()
-      return
-    makeRandomFile(numLines, outputFilePath + ".gz", bucket, prefix)
-    queue.task_done()
-    print "Finished processing {0}".format(outputFilePath)
-
+  gzip_file.close()
+  print 'os:', os.path.getsize(outputFilePath), ' gzip_file:',gzip_file.comp_bytes_written
+  return hashemi.hexdigest(), gzip_file.bytes_written, gzip_file.comp_bytes_written
 
 def div_rounded(x,y,prec=3):
   if y == 0: return 0
   return round(float(x)/y, prec)
 
 # This is just me being bored and playing with generators
-# This would do better as a callable class. Call your local
-# Python dealer for more information!
+# This would do better as a callable class. Contact your 
+# local Python dealer for more information!
 def file_complete():
   counter         = 0
   total_size      = 0
   total_comp_size = 0
   
   try:
-    #yield #first call is None
-    total_files = (yield)#(yield counter, total_size, total_comp_size)
+    total_files = (yield)
     while True:
       ret = (yield counter, total_size, total_comp_size)
       if ret:
@@ -162,17 +199,17 @@ def run(numLines, numFiles, outputFilePath, use_mp=True, max_workers=10, hash_ty
   print "Lines: {0}, Files: {1}, Base Name: {2}, Bucket: {3}, Prefix: {4}".format(numLines, numFiles, outputFilePath, S3_BUCKET, S3_PREFIX)
 
   callback = file_complete()
-  callback.send(None) # prime it?
+  callback.send(None) # prime it
   callback.send(numFiles)
 
   if use_mp:
     pool = multiprocessing.Pool(min(max_workers, numFiles))
 
   for i in range(numFiles):
-    kwargs = {numLines: numLines,
-              outputFilePath: outputFilePath + "." + str(i) + '.tsv.gz',
-              hash_type: hash_type,
-              comp_level: comp_level,
+    kwargs = {'numLines': numLines,
+              'outputFilePath': outputFilePath + "." + str(i) + '.tsv.gz',
+              'hash_type': hash_type,
+              'comp_level': comp_level,
               }
             
     if use_mp:
